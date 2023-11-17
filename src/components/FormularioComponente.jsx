f27aa7f8556920d993870e106058dd5eb136b32d
OMARROJASP <63993140+OMARROJASP@users.noreply.github.com>
2023-11-17 09:08:32 -0500

    formulario v1

--- src/components/FormularioComponente.jsx
+++ src/components/FormularioComponente.jsx
@@ -1,15 +1,38 @@
-export const FormularioComponente = () => {
+import {useState} from "react";
 
+export const FormularioComponente = () => {
+    const [formState, setFormState] = useState({
+        userName:'Omar Rojas Poma',
+        email: 'omarrojasp@gmail.com',
+        password: '123123'
+    })
+
+    const {userName, email, password} = formState
+
+    const onInputChange= ({target})=>{
+       const {name, value} = target
+        setFormState({
+            ...formState,
+            [name]: value
+        })
+    }
+
+    const onSubmit = (event) => {
+        event.preventDefault()
+        console.log(formState)
+    }
     return(
         <>
-            <form>
+            <form onSubmit={onSubmit}>
                 <div className="form-group">
-                    <label htmlFor="userName">Email address</label>
+                    <label htmlFor="userName">User Name</label>
                     <input
-                            type="email"
+                            type="text"
                             className="form-control"
                             name="userName"
                             placeholder="Enter your UserName"
+                            value={userName}
+                            onChange={onInputChange}
                     />
 
                 </div>
@@ -20,6 +43,8 @@ export const FormularioComponente = () => {
                         className="form-control"
                         name="email"
                         placeholder="Enter email"
+                        value={email}
+                        onChange={onInputChange}
                     />
                 </div>
                 <div className="form-group">
@@ -29,6 +54,8 @@ export const FormularioComponente = () => {
                         className="form-control"
                         name="password"
                         placeholder="Password"
+                        value={password}
+                        onChange={onInputChange}
                     />
                 </div>
                 <button
